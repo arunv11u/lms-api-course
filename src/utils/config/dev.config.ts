5a82eafb32c66cf605f8b4bdf9b7b0533ddf39a9
Arun Varadharajalu <arunvaradharajalu@gmail.com>
2024-09-19 23:05:10 -0400

    Update mongodb database name

--- src/utils/config/dev.config.ts
+++ src/utils/config/dev.config.ts
@@ -1,7 +1,7 @@
 export const devConfig = {
 	port: 3000,
 	mongodbURL: "mongodb+srv://cluster0.qwm8c.gcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-	mongodbDatabaseName: "learning-management-system",
+	mongodbDatabaseName: "lms-main",
 	messagingConsumerGroupId: "course-service-group",
 	bootstrapKafkaBroker: "my-cluster-kafka-bootstrap.default.svc.cluster.local:9092"
 };
